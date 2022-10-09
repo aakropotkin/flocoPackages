1eec2113928425422339d82d9ff1a489b5b6de5d
Alex Ameen <alex.ameen@tulip.co>
2022-10-09 15:41:35 -0500

    add all unscoped

--- registry/mkRegistryTree.nix
+++ registry/mkRegistryTree.nix
@@ -4,15 +4,16 @@
 , pkgsFor         ? floco.legacyPackages.${system}
 , buildEnv        ? pkgsFor.buildEnv
 , writeTextDir    ? pkgsFor.writeTextDir
-# Attrs { ident -> registry attrs }
+# { scope -> { bname -> registry attrs } }
 , targets         ? lib.importJSON ./npmjs.json
 , scope           ? "_"
 , flakeRegistries ? null
 }: let
-  writeRegistry = ident: reg: writeTextDir ident ( builtins.toJSON reg );
+  writeRegistry = ident: reg:
+    writeTextDir "${ident}.json" ( builtins.toJSON reg );
   genReg = ident: writeRegistry ident ( lib.libreg.flakeRegistryFromNpm ident );
-  regsForScope = scope: idents:
-    map ( i: genReg ( if scope == "_" then i else "@${scope}/${i}" ) ) idents;
+  regsForScope = scope: bnames:
+    map ( i: genReg ( if scope == "_" then i else "@${scope}/${i}" ) ) bnames;
   regs = if flakeRegistries != null then flakeRegistries else
          builtins.mapAttrs regsForScope targets;
 in buildEnv {
