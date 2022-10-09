6d2779087bf26fd8de790bf21a3011207dc8703d
Alex Ameen <alex.ameen@tulip.co>
2022-10-09 17:35:31 -0500

    generate registries

--- registry/mkRegistryTree.nix
+++ registry/mkRegistryTree.nix
@@ -6,17 +6,37 @@
 , writeTextDir    ? pkgsFor.writeTextDir
 # { scope -> { bname -> registry attrs } }
 , targets         ? lib.importJSON ./npmjs.json
-, scope           ? "_"
+, scope           ? "UNSCOPED"
 , flakeRegistries ? null
+, timestamp       ? toString builtins.currentTime
 }: let
+
+  isNs = x: builtins.elem x ["UNSCOPED" "unscoped" "" "unscoped/" "."];
+
+  # NOTE: these routines were written to accept `scope' as an argument.
+  # However, this file and its associated script intentionally target a single
+  # scope at once to limit resource consumption.
+  # Pay attention to =scope= vs =scope'=.
+  scopedirFor = s: if isNs s then ""          else "@${s}/";
+  localdirFor = s: if isNs s then "unscoped/" else "@${s}/";
+  outfileFor  = i: ( localdirFor ( dirOf i ) ) + ( baseNameOf i ) + ".json";
+  attrFor     = s: if isNs s then "UNSCOPED"  else lib.yank "@([^/@]+).*" s;
+
   writeRegistry = ident: reg:
-    writeTextDir "${ident}.json" ( builtins.toJSON reg );
+    writeTextDir ( outfileFor ident ) ( builtins.toJSON reg );
+
   genReg = ident: writeRegistry ident ( lib.libreg.flakeRegistryFromNpm ident );
-  regsForScope = scope: bnames:
-    map ( i: genReg ( if scope == "_" then i else "@${scope}/${i}" ) ) bnames;
+
+  regsForScope = scope': bnames:
+    map ( b: genReg "${scopedirFor scope'}${b}" ) bnames;
   regs = if flakeRegistries != null then flakeRegistries else
          builtins.mapAttrs regsForScope targets;
+
+  # Now we stope treating "scope" as an arg.
+  timestampFile = writeTextDir "${localdirFor scope}.timestamp"
+                               ( toString timestamp );
 in buildEnv {
-  name = "registry-tree";
-  paths = regs.${scope};
+  name = if isNs scope then "registry-tree-unscoped"
+         else "registry-tree-${scope}";
+  paths = regs.${attrFor scope} ++ [timestampFile];
 }
---
+++ registry/write-registry-tree.sh
@@ -0,0 +1,54 @@
+#! /usr/bin/env bash
+
+set -eu;
+
+SDIR="${BASH_SOURCE[0]%/*}";
+
+: "${NIX:=nix}";
+: "${JQ:=jq}";
+: "${CHMOD:=chmod}";
+: "${CP:=cp}";
+
+: "${SCOPE:=${1:-_}}";
+
+if test "$SCOPE" = "unscoped"; then
+  SCOPE="_";
+fi
+
+trap 'exit "$?"' HUP EXIT TERM INT QUIT;
+
+case "$SCOPE" in
+  --all)
+    for s in $( $JQ -r 'keys[]' "$SDIR/npmjs.json"; ); do
+      ${BASH_SOURCE[0]} "$s";
+    done
+    exit 0;
+  ;;
+  --missing)
+    for s in $( $JQ -r 'keys[]' "$SDIR/npmjs.json"; ); do
+      if { ! test -d "$SDIR/$s"; } ||
+         { test "$s" = "_" && test -d "$SDIR/unscoped"; };
+      then
+        ${BASH_SOURCE[0]} "$s";
+      fi
+    done
+    exit 0;
+  ;;
+  _)
+    FDIR="$SDIR/result/unscoped";
+    TDIR="$SDIR/unscoped";
+  ;;
+  *)
+    FDIR="$SDIR/result/@$SCOPE";
+    TDIR="$SDIR/@$SCOPE";
+  ;;
+esac
+
+$NIX build --impure -L -f ./mkRegistryTree.nix --argstr scope "$SCOPE";
+
+if test -d "$TDIR"; then
+  mv "$TDIR" "$TDIR~";
+fi
+
+$CP -LRf -- "$FDIR" "$TDIR";
+$CHMOD -R +w -- "$TDIR";
