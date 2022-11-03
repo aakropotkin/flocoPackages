46f271b0d4826827da6aac9e611faef908b5755b
Alex Ameen <alex.ameen@tulip.co>
2022-11-03 01:07:08 -0500

    bump lock

--- bin/rewrite-meta-with-fetchinfo.sh
+++ bin/rewrite-meta-with-fetchinfo.sh
@@ -1,14 +1,37 @@
 #! /usr/bin/env bash
+
 # Functional Draft.
 # Needs to be made into robust script.
 # Rewrites `meta.json' `fetchInfo' fields with optimized `fetchInfo'.
-jq '
+
+set -x
+
+: "${JQ:=jq}";
+: "${NIX:=nix}";
+
+if test "$#" -gt 0; then
+  PDIR="${1%/meta.*}";
+else
+  PDIR="$PWD";
+fi
+
+if ! test -r "$PDIR/fetchInfo.json"; then
+  "${BASH_SOURCE[0]%/*}/genFetchInfoFromMS" -s $PDIR/meta.*  \
+    > "$PDIR/fetchInfo.json";
+fi
+
+if ! test -r "$PDIR/meta.json"; then
+  $NIX eval --impure --json -f $PDIR/meta.nix > "$PDIR/meta.json";
+fi
+
+
+$JQ '
 with_entries(
   .key as $key|
   ( $fi|flatten|add ) as $fetchInfo|
   .value.fetchInfo|=(
     del(.hash)|
     del(.sha512)|
-    .+=$fechtInfo[$key]
+    .+=$fetchInfo[$key]
   )
-)' --slurpfile fi "${1%/meta.json}/fetchInfo.json" "${%/meta.json}/meta.json";
+)' --slurpfile fi "$PDIR/fetchInfo.json" "$PDIR/meta.json";
