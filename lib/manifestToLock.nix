473db7b29b6d2eedfd406142e0cb72e50cb99b6e
aameen-tulip <98349328+aameen-tulip@users.noreply.github.com>
2022-11-05 03:22:51 -0500

    Update manifestToLock.nix

--- lib/manifestToLock.nix
+++ lib/manifestToLock.nix
@@ -3,12 +3,12 @@
     type = "tarball";
     url  = dist.tarball;
   };
-  fetchInfo = removeAttrs ( builtins.fetchTree fetchInfo ) ["outPath"];
+  sourceInfo = removeAttrs ( builtins.fetchTree fetchInfo ) ["outPath"];
   scope = let
     m = builtins.match "@([^@/]+)/.*" name;
   in if m == null then null else builtins.head m;
 in {
   ident = name;
-  inherit version scope fetchInfo;
-  fetchInfo = fetchInfo // { inherit (fetchInfo) narHash; };
+  inherit version scope sourceInfo;
+  fetchInfo = fetchInfo // { inherit (sourceInfo) narHash; };
 }
