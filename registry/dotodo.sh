33e9cbdd57e8b96002099465ca44eeb1b055f495
Alex Ameen <alex.ameen@tulip.co>
2022-11-04 23:36:01 -0500

    lock a bunch of stuff

--- registry/dotodo.sh
+++ registry/dotodo.sh
@@ -4,17 +4,37 @@ export NIX_CONFIG='
 warn-dirty = false
 ';
 
-#for d in $( cat ./todo|grep -v '^#'|sort -u; ); do
-for d in $( head ./todo|grep -v '^#'|sort -u; ); do
+cleanup() {
+  if test ./bads; then
+    sort -u ./bads > ./bads~;
+    mv ./bads~ ./bads;
+  fi
+  if test ./dones; then
+    sort -u ./dones > ./dones~;
+    mv ./dones~ ./dones;
+  fi
+}
+
+for d in $( cat ./todo|grep -v '^#'|sort -u; ); do
   mkdir -p "$( dirname "$PWD/result/$d"; )";
   scope="${d%/*}";
   bname="${d#*/}";
+  ldir="@$scope";
+  if test "$ldir" = "@unscoped"; then ldir="unscoped"; fi
+  echo "$scope/$bname" >&2;
   nix eval --impure ".#tlocksTbJSON.$scope.\"$bname.json\"" --raw  \
     > "$PWD/result/$d.json"||                           \
   {
     rm "$PWD/result/$d.json";
     echo "$d" >> bads;
     echo "FAILED: $d" >&2;
+    cleanup;
     exit 1
   };
+  if ! test -r "$ldir/$bname.json"; then
+    mv "$PWD/result/$d.json" "$ldir/$bname.json";
+    git add "$ldir/$bname.json";
+  fi
+  echo "$d" >> ./dones;
 done 
+cleanup;
