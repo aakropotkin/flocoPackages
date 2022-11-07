93200872dde077138bc3356ac1f037e213f05a91
Alex Ameen <alex.ameen@tulip.co>
2022-11-06 22:52:19 -0600

    add @electron/get info

---
+++ info/electron/get/flake.nix
@@ -0,0 +1,22 @@
+{
+
+  inputs.packument.url   = "https://registry.npmjs.org/@electron/get?rev=39-ffbfc9b9507596e8f4af11453b882067";
+  inputs.packument.flake = false;
+
+  outputs = inputs: let
+    importJSON = f: builtins.fromJSON ( builtins.readFile f );
+    packument  = importJSON inputs.packument;
+    fetchInfo  = if ! builtins.pathExists ./fetchInfo.json then {} else
+                 importJSON ./fetchInfo.json;
+    latest'    = if ! ( packument ? dist-tags.latest ) then {} else {
+      latestVersion = packument.dist-tags.latest;
+      latest        = packument.versions.${packument.dist-tags.latest};
+    };
+  in {
+    scope = "@electron";
+    ident = "@electron/get";
+    ldir  = "info/electron/get";
+    inherit packument fetchInfo;
+  } // latest';
+
+}
