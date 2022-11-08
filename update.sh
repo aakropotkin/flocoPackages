806b6891e9f5bdc567004d7ed2b2c29f2f3a60b8
Alex Ameen <alex.ameen@tulip.co>
2022-11-08 13:06:38 -0600

    bump locks

--- update.sh
+++ update.sh
@@ -27,10 +27,22 @@ while test "$#" -gt 0; do
   shift;
 done
 
-$FIND "$ROOT_DIR" -name flake.lock           \
+# NOTE: we intentionally skip updating `registry/' and `info/' since those take
+# a long time to run.
+# They should be triggered manually.
+
+# Update packages
+$FIND "$ROOT_DIR/pkgs" -name flake.lock      \
   -execdir $NIX flake "$CMD" ${NARGS[@]} \;  \
   -execdir $BASH -c '
     if ! $NIX flake check; then
       $GIT restore ./flake.lock;
       echo "\nFailed to update: $PWD" >&2;
     fi' \; ;
+
+# Update top level
+$NIX flake $CMD "${NARGS[@]}" "$ROOT_DIR";
+if ! $NIX flake check "$ROOT_DIR"; then
+  $GIT restore "$ROOT_DIR/flake.lock";
+  echo "\nFailed to update: $PWD" >&2;
+fi
