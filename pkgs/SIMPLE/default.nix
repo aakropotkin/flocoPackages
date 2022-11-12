fabc1f2f1f746a243acd4c88f7f35bff504213dc
Alex Ameen <alex.ameen@tulip.co>
2022-11-11 22:46:08 -0500

    merge

--- pkgs/SIMPLE/default.nix
+++ pkgs/SIMPLE/default.nix
@@ -6,21 +6,46 @@
 , coreOv      ? import ../../overlay.nix
 , ov          ? nixpkgs.lib.composeExtensions depOv coreOv
 , system      ? builtins.currentSystem
-, pkgsFor     ? nixpkgs.legacyPackages.extend ov
+, pkgsFor     ? nixpkgs.legacyPackages.${system}.extend ov
 }: let
 
   simpleOv   = import ./overlay.nix;
   simplePkgs = pkgsFor.extend simpleOv;
 
-  flocoPkgs     = simplePkgs.flocoPackages;
-  flocoPkgsHier = lib.libattrs.pkgsAsAttrsets ( removeAttrs flocoPkgs [
+  flocoPkgs   = simplePkgs.flocoPackages;
+  noInternals = removeAttrs flocoPkgs [
+    "extend"
+    "__unfix__"
     "__internalSimple"
     "__internalBins"
     "__internalInst"
     "__internalGyp"
     "__internalInfo"
     "__internal"
-  ] );
+  ];
+
+  flocoPkgsHier = let
+    proc = acc: key: let
+      m = builtins.match "(@([^@/]+)/)?([^@/]+)/(.*)" key;
+      version = builtins.elemAt m 3;
+      bname   = builtins.elemAt m 2;
+      scope'  = builtins.elemAt m 1;
+      scope   = if scope' == null then "unscoped" else scope';
+      fl      = lib.toLower ( builtins.substring 0 1 bname );
+      addB    = prev: prev // {
+        ${bname} = ( prev.${bname} or {} ) // {
+          ${version} = flocoPkgs.${key};
+        };
+      };
+    in if m == null then throw "bad key: '${key}'" else acc // {
+      ${scope} = let
+        prevS = acc.${scope} or {};
+        prev  = if scope' == null then prevS.${fl} or {}
+                                  else prevS;
+      in if scope' == null then prevS // { ${fl} = addB prev; }
+                           else prevS // ( addB prev );
+    };
+  in builtins.foldl' proc {} ( builtins.attrNames noInternals );
 
 in {
 
