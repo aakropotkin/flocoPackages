576dbe5de434d0bf71ef3bb321daafdfb5fb5658
Alex Ameen <alex.ameen@tulip.co>
2022-11-14 17:24:30 -0600

    uuid bin meta

---
+++ pkgs/BINS/unscoped/t/typescript/meta.nix
@@ -0,0 +1,9 @@
+{ version }: {
+
+  bin = let
+    extra = if 0 < ( builtins.compareVersions "1.5.0" version ) then {} else {
+      tsserver = "bin/tsserver";
+    };
+  in { tsc = "bin/tsc"; } // extra;
+
+}
---
+++ pkgs/BINS/unscoped/u/uuid/meta.nix
@@ -0,0 +1,10 @@
+{ version }: {
+
+  bin = if 0 < ( builtins.compareVersions "3.0.0" version ) then {} else
+        if 0 < ( builtins.compareVersions "7.0.0" version ) then {
+          uuid = "bin/uuid";
+        } else {
+          uuid = "dist/bin/uuid";
+        };
+
+}
