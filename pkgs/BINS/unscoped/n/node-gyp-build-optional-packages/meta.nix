362b70204106cb0b55e30a9b89d0e1995d45c7e5
Alex Ameen <alex.ameen@tulip.co>
2022-11-14 19:29:03 -0600

    fix conditionals in node-gyp-biuld-optional-packages

--- pkgs/BINS/unscoped/n/node-gyp-build-optional-packages/meta.nix
+++ pkgs/BINS/unscoped/n/node-gyp-build-optional-packages/meta.nix
@@ -1,15 +1,21 @@
 { version }: {
-  bin = if version == "4.3.0" then {
-    node-gyp-build          = "bin.js";
-    node-gyp-build-optional = "optional.js";
-    node-gyp-build-test     = "build-test.js";
-  } else builtins.elem version ["4.3.1" "4.3.2" "5.0.0" "5.0.1" "5.0.2"] {
-    node-gyp-build-optional-packages = "bin.js";
-    node-gyp-build-optional          = "optional.js";
-    node-gyp-build-test              = "build-test.js";
-  } else builtins.elem version ["4.3.3" "4.3.4" "4.3.5" "5.0.3"] {
-    node-gyp-build-optional-packages          = "bin.js";
-    node-gyp-build-optional-packages-optional = "optional.js";
-    node-gyp-build-optional-packages-test     = "build-test.js";
-  };
+  bin =
+  if version == "4.3.0" then
+    {
+      node-gyp-build          = "bin.js";
+      node-gyp-build-optional = "optional.js";
+      node-gyp-build-test     = "build-test.js";
+    }
+  else if builtins.elem version ["4.3.1" "4.3.2" "5.0.0" "5.0.1" "5.0.2"] then
+    {
+      node-gyp-build-optional-packages = "bin.js";
+      node-gyp-build-optional          = "optional.js";
+      node-gyp-build-test              = "build-test.js";
+    }
+  else #if builtins.elem version ["4.3.3" "4.3.4" "4.3.5" "5.0.3"] then
+    {
+      node-gyp-build-optional-packages          = "bin.js";
+      node-gyp-build-optional-packages-optional = "optional.js";
+      node-gyp-build-optional-packages-test     = "build-test.js";
+    };
 }
