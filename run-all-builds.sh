22889985ba77654f20eb479a86c12662d2e22956
Alex Ameen <alex.ameen@tulip.co>
2022-11-14 19:20:27 -0600

    fix hexy meta.json

--- run-all-builds.sh
+++ run-all-builds.sh
@@ -11,8 +11,8 @@ trap '_es="$?"; exit "$_es";' HUP TERM EXIT INT QUIT;
 targets=( $( $NIX flake show --json|$JQ ".packages[\"$SYSTEM\"]|keys[]"; ) );
 
 for t in "${targets[@]}"; do
-  echo "Building Impure:  $t";
-  $NIX build $NIX_FLAGS --impure ".#$t";
+  #echo "Building Impure:  $t";
+  #$NIX build $NIX_FLAGS --impure ".#$t";
   echo "Building Pure:  $t";
   $NIX build $NIX_FLAGS ".#$t";
 done
